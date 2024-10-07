b5e9805fcaf9149bfd48818bd9c85f95155b3b7d
Beqavardukadze <beqa.vardukadze888@gmail.com>
2024-10-07 12:59:08 +0200

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,11 @@
+name = "Beqa"
+
+
+surname = "vardukadzea"
+age = 17
+height = 182
+knows_proggraming = False
+
+
+
+print(name+surname,"age" + str(17),"height" + str(182),"knows_proggraming" + str(False) )
