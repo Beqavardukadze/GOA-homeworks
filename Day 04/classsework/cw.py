4aec58a976b910ac07126f2386ab43a48df514c0
Beqavardukadze <beqa.vardukadze888@gmail.com>
2024-10-16 20:29:41 +0200

    second commit

---
+++ Day 04/classsework/cw.py
@@ -0,0 +1,44 @@
+name = "beqa" #string
+
+
+num = 1000 #integer
+
+num2 = 1000.50 #float
+
+
+
+name = "beqa"
+surname = "vardukadze"
+adress = "caen"
+
+
+print(name)
+print(surname)
+print(adress)
+
+print("my name is " + name)
+
+
+name= "beqa"
+num1 = 10
+num2 = 20.0
+
+print("GOA BEST")
+print("10")
+
+# მონაცემთა  ტიპი data type
+# string - str - აღნიშნავს ბრჭლალებში მოქცეულ ტექსტს
+# integer - int - აღნიშნავს მთელ რიცხვს 
+# float - float - ათწილად რიცხვებს
+
+
+# konkatinacia - concatination - stringebis sheertebas
+print(name + surname)
+print("beqa" + "vardukadze")
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
