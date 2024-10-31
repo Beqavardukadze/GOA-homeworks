4d4c72a15feaf410bb9c783659aca84fe5bb3e1a
Beqavardukadze <beqa.vardukadze888@gmail.com>
2024-10-31 15:55:04 +0100

    second commit

---
+++ level 08/homework/hm1.py
@@ -0,0 +1,11 @@
+a = 5
+b = "10"
+result = a + b
+print("Result:", result)
+
+# kodma dawera erori mesame xazze imitom rom aklia brchyalebi
+
+a = 5
+b = "10"
+result = "a" + "b"
+print("Result:", result)
