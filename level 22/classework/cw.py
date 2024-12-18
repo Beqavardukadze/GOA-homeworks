84c1d3d2ccbec3415fc12189e14a2b01f5d08b61
Beqavardukadze <beqa.vardukadze888@gmail.com>
2024-12-18 21:12:24 +0100

    second commit

---
+++ level 22/classework/cw.py
@@ -0,0 +1,21 @@
+
+# sum = 0
+# for i in range(4):
+#     sum = sum + 1
+
+
+
+# sum = int(input("enter number: "))
+# sum1 = 0
+# for i in range(sum):
+#     sum1 += i
+
+
+# print(sum1)
+
+
+
+
+
+
+
