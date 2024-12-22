3a30dc593d6c078499377db0f361f0a11a260d75
Beqavardukadze <beqa.vardukadze888@gmail.com>
2024-12-22 21:23:45 +0100

    second commit

--- level 22/homework/hw1.py
+++ level 22/homework/hw1.py
@@ -1,2 +1,9 @@
 # 1) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი ერთიდან ამ რიცხვამდე დაბეჭდეთ ყველა რიცხვის ჯამი
 
+run = int(input("enter your number: "))
+
+sum = 0
+
+for i in range(run):
+    sum += i
+print(sum)
