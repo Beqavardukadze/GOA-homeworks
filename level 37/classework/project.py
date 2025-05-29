982a8089dc1c1cdc4a5485f1693559b671597f4f
Beqavardukadze <beqa.vardukadze888@gmail.com>
2025-05-29 14:24:59 +0200

    first commit

---
+++ level 37/classework/project.py
@@ -0,0 +1,17 @@
+print("-- sesxis ageba --- ")
+
+print("--------------------- ")
+
+
+print("1) ipotequri sesxi")
+print("........................")
+print("2) samoxmareblo sesxi")
+print("........................")
+print("3) avto sesxi")
+print("........................")
+
+print("     ")
+
+print("sesxis limiti aris 1000000")
+
+print("..........................")
